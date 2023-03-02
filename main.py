9a33952b65241dd2e7d155ddca42fa368c8c01ef
Vishnukumar Cheruku <96327518+VISHCGIT@users.noreply.github.com>
2023-03-02 22:19:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd
 #from sklearn.model_selection import train_test_split
 #from sklearn.preprocessing import  LabelEncoder
-from miscfunctions import get_best_model
+#from miscfunctions import get_best_model
 import numpy as np
 import re
 import datetime
 st.header("Rossmann Sales Prediction App")
 
 # load model
-tf.keras.backend.clear_session()
+#tf.keras.backend.clear_session()
 input_shape = (64, 20, 1)
-print("input_shape: ",input_shape)
-best_model = get_best_model(input_shape, 264)
-best_model.run_eagerly = True
-best_model.load_model("best_model_cnn_spect.hdf5")
+st.write("input_shape: ",input_shape)
+#best_model = get_best_model(input_shape, 264)
+#best_model.run_eagerly = True
+#best_model.load_model("best_model_cnn_spect.hdf5")
