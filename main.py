2caef6f5ba5b0b2b9c662a0ecba314a99585207a
Vishnukumar Cheruku <96327518+VISHCGIT@users.noreply.github.com>
2023-03-02 22:30:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #from sklearn.model_selection import train_test_split
 #from sklearn.preprocessing import  LabelEncoder
-from miscfunctions import get_best_model
+#from miscfunctions import get_best_model
 import numpy as np
 import re
 import datetime
@@ -27,6 +27,33 @@ st.header("Rossmann Sales Prediction App")
 tf.keras.backend.clear_session()
 input_shape = (64, 20, 1)
 st.write("input_shape: ",input_shape)
+def get_best_model(input_shape, num_classes):
+    model=Sequential()
+
+    model.add(Conv2D(32, (3,3),activation='relu',input_shape=input_shape))
+    model.add(MaxPooling2D(2))
+
+    model.add(Conv2D(64, (3,3), activation="relu"))   # extra pad
+    model.add(MaxPooling2D((2,2)))                    #extra pad
+
+    model.add(Conv2D(128, (3,3), activation="relu"))  # extra pad
+    model.add(MaxPooling2D((2,2), padding='same'))
+
+    model.add(Conv2D(128, (3,3), activation="relu", padding='same'))
+    model.add(MaxPooling2D((2,2), padding='same'))
+
+    model.add(BatchNormalization())#act
+    model.add(Flatten())
+    model.add(Dropout(0.2)) #act
+    model.add(Dense(512,activation='relu'))
+    model.add(Dense(num_classes, activation='sigmoid'))
+    #model.summary()
+
+    opt = optimizers.SGD(lr=1e-4, momentum=0.9)
+    model.compile(loss ='binary_crossentropy', 
+                  optimizer = 'adam',
+                  metrics = ['accuracy', micro_f1])
+    return model
 best_model = get_best_model(input_shape, 264)
 st.write(best_model.summary())
 best_model.run_eagerly = True
