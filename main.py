f39bd8f99d0f96668a62eed4d3974e017931b35a
Vishnukumar Cheruku <96327518+VISHCGIT@users.noreply.github.com>
2023-03-02 22:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #from sklearn.model_selection import train_test_split
 #from sklearn.preprocessing import  LabelEncoder
-#from miscfunctions import get_best_model
+from miscfunctions import get_best_model
 import numpy as np
 import re
 import datetime
